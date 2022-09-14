33c04696c156930693ee85d3e36f5dc381bb2002
niyera <nilaniyera@gmail.com>
2022-09-14 09:32:47 +0200

    debug error in locationController

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    protected $table="locations";
+    protected $table="location";
     protected $fillable = ['date_location','montant_total','client_id'];
 }
